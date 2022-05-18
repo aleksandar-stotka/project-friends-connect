e19d4ccd683807db77cd8e1394a4c21fb611ae78
aleksandar angelevski <stotka123@yahoo.com>
2022-05-18 09:38:55 +0200

    set-uploadPath

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXFoaejA_rzZZ1LjuPz3c8CEYYBMGjsv0",
@@ -16,7 +17,8 @@ firebase.initializeApp(firebaseConfig);
 ///i it
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
