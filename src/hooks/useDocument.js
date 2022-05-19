6e273aca152cc8da74a1e45c4a2e36bf1960ecb8
aleksandar angelevski <stotka123@yahoo.com>
2022-05-19 09:07:52 +0200

    work-on-useDocuments

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  //real data
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id);
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        if (snapshot.data()) {
+          setDocument({
+            ...snapshot.data(),
+            id: snapshot.id,
+          });
+          setError(null);
+        } else {
+          setError("no such document exists");
+        }
+      },
+      (err) => {
+        console.log(err);
+        setError("failed to get document");
+      }
+    );
+    return () => unsubscribe();
+  }, [collection, id]);
+
+  return { document, error };
+};
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,22 @@
 import "./Project.css";
-
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 import React from "react";
 
 function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">loading...</div>;
+  }
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
 
 export default Project;
