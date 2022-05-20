dc48f23c4004bf39fe5e7a774b3df848e68f1108
aleksandar angelevski <stotka123@yahoo.com>
2022-05-21 00:44:51 +0200

    currentFilter-done

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -8,9 +8,40 @@ import ProjectFilter from "./ProjectFilter";
 import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Dashboard() {
-  const [currentFilter, setCurrentFilter] = useState("all");
   const { documents, error } = useCollection("projects");
+  const [currentFilter, setCurrentFilter] = useState("all");
   const { user } = useAuthContext();
+
+  ////if we have document
+
+  const projects = documents
+    ? documents.filter((document) => {
+        // eslint-disable-next-line default-case
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assingnedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+        }
+      })
+    : null;
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
   console.log(documents);
 
   /* switch (currentFilter) {
@@ -32,10 +63,6 @@ function Dashboard() {
       
     }*/
 
-  const changeFilter = (newFilter) => {
-    setCurrentFilter(newFilter);
-  };
-
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
@@ -46,7 +73,7 @@ function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
