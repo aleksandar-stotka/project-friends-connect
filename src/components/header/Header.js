4859388a5957ae1dedc3e96255251600f7242092
aleksandar angelevski <aangelevski6@gmail.com>
2022-10-08 02:45:51 +0200

    set-slide

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,16 +2,47 @@ import React from 'react'
 import './Header.css'
 import "aos/dist/aos.css";
 import Aos from "aos";
+import {useState, useEffect} from 'react'
+import "aos/dist/aos.css"
 
 
 const Header = () => {
+  const [newBack, setNewBack] = useState(true)
+ 
+  const newBackground = async () => {
+    try {
+      setTimeout(() => {
+        setNewBack(false);
+        console.log('set')
+      }, 4000);
+    } catch (err) {
+      console.log(err);
+    }
+  };;
+  
+  useEffect(() => {
+    newBackground()
+    Aos.init({ duration: 2500 });
+  
+},[newBack])
+
+///////////////////
   return <>
-     <div className="header">
-      <div className='header-content'>
-      <h1 className='text-yellow-300'>Create your Project</h1>
-      </div>
+  {newBack && <div className="test">
+        test
+    </div>}
+    {!newBack && <div className="header" data-aos="flip-left" >
+    <div className="flesh-content"> </div>
+    <div className="header-content"> <h1 >Create your Project</h1></div>
       
-    </div>
+     
+      
+    </div>}
+      
+ 
+     
+
+     
         
   </>
   
