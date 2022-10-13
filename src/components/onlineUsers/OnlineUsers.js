8947326826410acfd59056e9f7197a4192fdd9e6
aleksandar angelevski <aangelevski6@gmail.com>
2022-10-13 16:40:37 +0200

    work-on-online-users

--- src/components/onlineUsers/OnlineUsers.js
+++ src/components/onlineUsers/OnlineUsers.js
@@ -1,11 +1,13 @@
 import "./OnlineUsers.css";
 import { useCollection } from "../../hooks/useCollection";
-
+import { Link } from "react-router-dom";
 import React from "react";
 import Avatar from "../avatar/Avatar";
 
 function OnlineUsers() {
   const { documents, error } = useCollection("users");
+ 
+ console.log(documents,'online users')
 ///////////////////////
   return (
     <div className="user-list">
@@ -13,11 +15,14 @@ function OnlineUsers() {
       {error && <div className="error">{error}</div>}
       {documents &&
         documents.map((user) => (
+         // eslint-disable-next-line react/jsx-no-undef
+       
           <div key={user.id} className="user-list-item">
             {user.online && <span className="online-user"></span>}
             <span>{user.displayName}</span>
             <Avatar src={user.photoURL} />
           </div>
+        
         ))}
     </div>
   );
