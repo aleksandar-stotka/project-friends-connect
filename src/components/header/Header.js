a121fb1dcd7bfc8d84ce45b22442df9823b3f726
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-09-26 18:36:24 -0700

    delete-slide-header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,10 +4,8 @@ import "aos/dist/aos.css";
 import Aos from "aos";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
-import { useGlobalContext } from "../../globalContext/context";
 
 const Header = () => {
-  const { newBack } = useGlobalContext();
 
   useEffect(() => {
     Aos.init({ duration: 2500 });
@@ -16,21 +14,8 @@ const Header = () => {
   ///////////////////
   return (
     <>
-      {newBack && (
-        <div className="test">
-          <div className="photo"></div>
-          <div className="description" style={{margin:"4px"}}>
-            <p>
-              Hi , my name is Aleksandar and i am Front End Developer / from
-              Macedonia with year and half of experience. I am a very passionate
-              and enthusiast person, i want to learn new things everyday. I
-              started learning programming focusing on the Front End programming
-              languages and also i have experience with some Devops segments.
-            </p>
-          </div>
-        </div>
-      )}
-      {!newBack && (
+     
+    
         <div className="header" data-aos="flip-left">
           <div className="flesh-content"> </div>
 
@@ -40,7 +25,6 @@ const Header = () => {
             <h2>for better organization and teams workplace</h2>
           </div>
         </div>
-      )}
     </>
   );
 };
