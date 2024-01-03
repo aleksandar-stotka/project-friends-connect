d57ea74248627adfc07152f5c76caa6373767d3e
aleksandar-stotka <aangelevski6@gmail.com>
2024-01-03 16:28:25 +0100

    for-deploy

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -14,7 +14,7 @@ function Signup() {
 
   const handleFileChange = (e) => {
     setThumbnail(null);
-    let selected = e.target.files[0];P
+    let selected = e.target.files[0];
     if (!selected) {
       setThumbnailError("Please select a file");
       return;
