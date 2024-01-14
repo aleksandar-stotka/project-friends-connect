30ad6ae1e9f945e779125d78e67f57ca95d87aa2
aleksandar-stotka <aangelevski6@gmail.com>
2024-01-14 22:02:27 +0100

    fix-userList

--- src/pages/personalProjects/UserProjects.js
+++ src/pages/personalProjects/UserProjects.js
@@ -25,14 +25,14 @@ const UserProjects = () => {
         {userMapDocuments.map(project => (  // Safely mapping with an initialized array
           <Link className="max-w-lg rounded overflow-hidden shadow-lg p-5 " to={`/projects/${project.id}`} key={project.id}>
             <h2 className='text-white text-2xl font-bold mb-2'>{project.name}</h2>
-            <p className='text-blue-100'>By: {project.createdBy.displayName}</p>
+            <p className='text-blue-100 font-semibold'>By: {project.createdBy.displayName}</p>
             <p className='text-blue-200'>{project.details}</p>
             <p className='font-bold text-blue-200'>Due by: {project.dueDate.toDate().toDateString()}</p>
             <div>
               <ul>
                 <h2 className='text-cyan-500 font-bold'>Assigned Users List:</h2>
                 {project.assingnedUsersList?.map((user) => (  // Optional chaining for safe access
-                  <li key={user.photoURL}>
+                  <li className="flex justify-center" key={user.photoURL}>
                     <h4 className='text-blue-200'><br></br> {user.displayName}</h4>  
                     <Avatar src={user.photoURL} />
                   </li>
